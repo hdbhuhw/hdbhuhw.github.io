a6b0764177ac302a08cbe73b918cba181595fbe4
hdbhuhw <144400990+hdbhuhw@users.noreply.github.com>
2023-09-08 09:44:26 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,19 @@
 let myButton = document.querySelector("button");
 let myHeading = document.querySelector("h1");
 function setUserName() {
-  let myName = prompt("请输入你的名字。");
+  let myName = prompt("请输入你的狗牌。");
   if (!myName) {
     setUserName();
   } else {
     localStorage.setItem("name", myName);
-    myHeading.textContent = "你在干什么，" + myName;
+    myHeading.textContent = "你在看什么你个老色批，" + myName;
   }
 }
 if (!localStorage.getItem("name")) {
   setUserName();
 } else {
   let storedName = localStorage.getItem("name");
-  myHeading.textContent = "你在干什么，" + storedName;
+  myHeading.textContent = "你在看什么你个老色批，" + storedName;
 }
 myButton.onclick = function () {
   setUserName();
